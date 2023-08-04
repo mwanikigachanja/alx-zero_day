bb6dd2807524c536a94c6ff16037ac0d78ab3be2
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-04 09:37:30 -0700

    UI Update 1

--- CMG_blackjack/frontend/ui.py
+++ CMG_blackjack/frontend/ui.py
@@ -6,6 +6,8 @@ class BlackjackUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Blackjack Game")
+        self.player_chips = 100
+        self.bet_amount = 0
         # Add UI components and logic here
         
         # Initialize game logic and score manager
@@ -24,6 +26,19 @@ class BlackjackUI:
 
         self.stand_button = tk.Button(root, text="Stand", command=self.player_stand, state=tk.DISABLED)
         self.stand_button.pack()
+
+        self.double_button = tk.Button(root, text="Double Down", command=self.double_down, state=tk.DISABLED)
+        self.double_button.pack()
+
+        self.split_button = tk.Button(root, text="Split", command=self.split, state=tk.DISABLED)
+        self.split_button.pack()
+
+        self.insurance_button = tk.Button(root, text="Insurance", command=self.take_insurance, state=tk.DISABLED)
+        self.insurance_button.pack()
+
+        self.animate_button = tk.Button(root, text="Animate", command=self.animate_dealing)
+        self.animate_button.pack()
+
 
         self.quit_button = tk.Button(root, text="Quit", command=root.quit)
         self.quit_button.pack()
@@ -69,6 +84,33 @@ class BlackjackUI:
         scores = self.score_manager.get_scores()
         score_text = "\n".join(f"{name}: {score}" for name, score in scores)
         messagebox.showinfo("Scores", score_text)
+
+    def double_down(self):
+        if self.player_chips >= self.bet_amount * 2:
+            self.bet_amount *= 2
+            self.hit_button.config(state=tk.DISABLED)
+            self.stand_button.config(state=tk.DISABLED)
+            self.player_hit()
+            self.player_stand()
+            self.game.double_down()
+
+    def split(self):
+        # Logic for splitting pairs (requires more detailed game implementation)
+        self.game.split()
+
+    def take_insurance(self):
+        # Logic for taking insurance against dealer's potential blackjack
+        self.game.take_insurance()
+
+    def update_chips_label(self):
+        self.chips_label.config(text="Chips: " + str(self.player_chips))
+        self.bet_label.config(text="Bet: " + str(self.bet_amount))
+    
+    def animate_dealing(self):
+        # Implement card dealing animation using animation frames
+
+    def play_sound(self, sound_file):
+        # Implement sound playing logic
 
 def main():
     root = tk.Tk()
