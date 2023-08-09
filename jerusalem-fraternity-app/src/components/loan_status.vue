41a39ced6c29ddba13ab5b9f4070ef74fc2b16d8
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-09 06:11:56 -0700

    Loan Status  init"

---
+++ jerusalem-fraternity-app/src/components/loan_status.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Loan Status
+    </v-card-title>
+    <v-card-text>
+      <div v-if="hasLoan">
+        <p>You have an active loan.</p>
+        <p>Loan Amount: {{ loanAmount }}</p>
+      </div>
+      <div v-else>
+        <p>You don't have an active loan.</p>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    hasLoan: Boolean,
+    loanAmount: Number,
+    // Add more props as needed
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
+
