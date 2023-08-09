5e921e7a3615777a22333891efe5014c33464b8c
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-09 06:03:59 -0700

    Account manager module init

---
+++ jerusalem-fraternity-app/src/components/acc_man.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Account Management
+    </v-card-title>
+    <v-card-text>
+      <v-form @submit.prevent="updateAccount">
+	<v-text-field v-model="id_no" label="ID Number"></v-text-field>
+        <v-text-field v-model="firstName" label="First Name"></v-text-field>
+        <v-text-field v-model="lastName" label="Last Name"></v-text-field>
+	<v-text-field v-model="p_num" label="Phone Number"></v-text-field>
+	<v-text-field v-model="bal_0" label="Current Balance"><v-text-field>
+        <!-- Add more fields for account management -->
+        <v-btn type="submit" color="primary">Update Account</v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id_no: '',
+      firstName: '',
+      lastName: '',
+      p_num: '',
+      bal_0: '',
+      // Add more data properties for account management
+    };
+  },
+  methods: {
+    updateAccount() {
+      // Implement the logic to update the account
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
+
