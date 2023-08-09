05026ebeb7e8c75a91a3b2c43d8e34a074613a65
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-09 06:07:51 -0700

    dashboard init

---
+++ jerusalem-fraternity-app/src/views/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <account-management></account-management>
+    <loan-status></loan-status>
+    <payments></payments>
+    <!-- Add more components as needed -->
+  </div>
+</template>
+
+<script>
+import AccountManagement from '@/components/acc_man.vue';
+import LoanStatus from '@/components/loan_status.vue';
+import Payments from '@/components/payments.vue';
+
+export default {
+  components: {
+    AccountManagement,
+    LoanStatus,
+    Payments,
+  },
+};
+</script>
+
