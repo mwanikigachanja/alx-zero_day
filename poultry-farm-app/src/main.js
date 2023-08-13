e59c38da0479324e9a385d705361183174aa3675
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-13 17:09:18 +0300

    Styling and more stocks admin

--- poultry-farm-app/src/main.js
+++ poultry-farm-app/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 createApp(App).mount('#app')
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify: new Vuetify(),
+  // ...
+}).$mount('#app');
---
+++ poultry-farm-app/src/views/StocksView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Stocks Module</h1>
+    <StocksList :stocks="stocks" />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState('stocks', ['stocks'])
+  },
+  created() {
+    this.fetchStocks();
+  },
+  methods: {
+    ...mapActions('stocks', ['fetchStocks'])
+  }
+};
+</script>
+
