edddee8d1c81edb764c0601801e6c3979487aee8
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-16 19:41:37 +0300

    More models

--- paroko_web/donation/models.py
+++ paroko_web/donation/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
+class Donation(models.Model):
+    user = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.user.username} - {self.amount}"
 
