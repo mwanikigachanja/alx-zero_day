c0e469577d8a550607a2c0dff9b7cd37fe7a8455
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-19 13:09:46 +0300

    Refining: 1

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,67 +1,30 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _printf - Entry point
- * Desc: Entry
- *@format: pointer
- * Return: On success.
- */
+ * _printf - Produces output according to a format...
+ * @format: Is a character string. The format string...
+ * is composed of zero or more directives
+ *
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-	va_list mylist;
-	unsigned int i = 0, j = 0;
+	int size;
+	va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-	va_start(mylist, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '%')
-			{	_putchar('%');
-				j++;
-				i++;
-			}
-			else if (get_op_func(format, i + 1) != NULL)
-			{	j += (get_op_func(format, i + 1))(mylist);
-				i++;
-			}
-			else
-			{	_putchar(format[i]);
-				j++;
-			}
-		}
-		else
-		{	_putchar(format[i]);
-			j++;
-		}
-	}
-	va_end(mylist);
-	return (j);
-}
-/**
- * get_op_func - Entry function
- * @s: operator
- * @pos: position
- * Return: function
- */
-int (*get_op_func(const char *s, int pos))(va_list)
-{
-	print_fun ops[] = {
-		{"c", print_single_char},
-		{"s", print_string_char},
-		{"d", print_decimal},
-		{"i", print_decimal},
-		{NULL, NULL}};
-	int k;
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
 
-	for (k = 0; ops[k].op != NULL; k++)
-	{
-		if (ops[k].op[0] == s[pos])
-		{
-			return (ops[k].f);
-		}
-	}
-	return (NULL);
+	va_start(args, format);
+	size = handler(format, args);
+
+	_putchar(-1);
+	va_end(args);
+
+	return (size);
 }
