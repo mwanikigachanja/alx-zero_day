1489061536ec0e7fc2a802cf22c798a22e390856
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-09-09 07:14:17 -0700

    test

--- C/file_handling.c
+++ C/file_handling.c
@@ -4,8 +4,11 @@
 
 int main()
 {
+	char file_name[255];
+	printf("Enter your file name:");
+	scanf("Enter file name:%s\n",file_name);
 	FILE *fp;
-	fp = fopen("first_file.txt","w");
+	fp = fopen("{file_name}.txt","w");
 	char ch[2000];
 	fprintf(fp,"%s","First File!!\n\nWhat a surprise, C is a world of wonders!\n\nSee you in the next phase.\n");
 
