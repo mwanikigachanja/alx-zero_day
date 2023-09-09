36753177d90b5c445e556779b9d011a9f4529a88
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-09-09 07:36:40 -0700

    Files advaced

--- C/file_handling.c
+++ C/file_handling.c
@@ -4,13 +4,16 @@
 
 int main()
 {
+
 	char file_name[255];
-	printf("Enter your file name:");
-	scanf("Enter file name:%s\n",file_name);
 	FILE *fp;
-	fp = fopen("{file_name}.txt","w");
+	printf("Enter your file name:");
+	scanf("%s",file_name);
+
+	fp = fopen(file_name,"w");
 	char ch[2000];
-	fprintf(fp,"%s","First File!!\n\nWhat a surprise, C is a world of wonders!\n\nSee you in the next phase.\n");
+	fprintf(fp,"First File name is %s !!\n\nWhat a surprise, C is a world of wonders!\n\nSee you in the next phase.\n",file_name);
+	fprintf(fp, "Your file %s is ready for use.\n",file_name);
 
 	printf("%s",fgets(ch,500,fp));
 
